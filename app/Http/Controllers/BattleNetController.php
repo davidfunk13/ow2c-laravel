f4445313c8ae033901e7906cb5155b46c8e4e267
David Funk <davefunk135@gmail.com>
2023-09-23 18:45:15 -0600

    returing cookie

--- app/Http/Controllers/BattleNetController.php
+++ app/Http/Controllers/BattleNetController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cookie;
 use Illuminate\Support\Facades\Log;
 use Laravel\Socialite\Facades\Socialite;
 
@@ -19,20 +20,17 @@ class BattleNetController extends Controller
     {
         try {
             $socialiteUser = Socialite::driver('battle_net')->user();
-            $newUser = User::create([
-                'name' => $socialiteUser->attributes['name'],
-                'sub' => $socialiteUser->attributes['sub'],
-            ]);
 
-            $newUser->save();
+            $user = User::firstOrCreate(['sub' => $socialiteUser->attributes['sub']], ['name' => $socialiteUser->attributes['name']]);
+
+            $token = $user->createToken('access-token')->plainTextToken;
+
+            $cookie = Cookie::make('token', $token, 60, null, null, false, true); // 60 minutes expiration
+
         } catch (Exception $e) {
             Log::error($e->getMessage());
         }
 
-        // $token = $user->token;
-
-        // Implement your logic here: log in the user, create an account, etc.
-
-        return redirect('http://localhost:5174/');
+        return redirect('http://localhost:5174/')->withCookie($cookie);
     }
 }
